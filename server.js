90c652147d2fe5b42ea3cff2f9153e3da40958fa
alanjso <alanoliveira1512@gmail.com>
2020-11-22 03:40:10 -0300

    Server rodando apenas na porta HTTP

--- server.js
+++ server.js
@@ -9,27 +9,27 @@ const eventEmit = require('./api/util/eventEmmiter');
 require('./api/util/schedule');
 require('./api/conversa/Telegram/conversa-telegram-service')();
 
-var https = require('https').createServer({
-  key: fs.readFileSync('./certificados-ssl/flexchannel.key'),
-  cert: fs.readFileSync('./certificados-ssl/flexchannel.crt'),
-  ca: [
-    fs.readFileSync('./certificados-ssl/flexchannel_b.crt')
-  ]
-}, server);
+// var https = require('https').createServer({
+//   key: fs.readFileSync('./certificados-ssl/flexchannel.key'),
+//   cert: fs.readFileSync('./certificados-ssl/flexchannel.crt'),
+//   ca: [
+//     fs.readFileSync('./certificados-ssl/flexchannel_b.crt')
+//   ]
+// }, server);
 
 var http = require('http').createServer(server);
 
-let io = require('socket.io')(https);
-io.adapter(redisAdapter({ host: config.get('database_redis_ip'), port: 6379 }));
+let io = require('socket.io')(http);
+// io.adapter(redisAdapter({ host: config.get('database_redis_ip'), port: 6379 }));
 // io.set("transports", ["websocket"]);
 io.set('heartbeat timeout', 4000000);
 io.set('heartbeat interval', 2000000);
 require('./api/conversa/v2/conversa-socket-service')(io);
 // const asterisk = require('./api/asterisk/connections');
 
-https.listen(config.get("port"), () => {
-  log.success(' ==> Servidor HTTPS Rodando na Porta: ' + config.get("port"))
-});
+// https.listen(config.get("port"), () => {
+//   log.success(' ==> Servidor HTTPS Rodando na Porta: ' + config.get("port"))
+// });
 
 http.listen(config.get("port_http"), () => {
   log.success(' ==> Servidor HTTP Rodando na Porta: ' + config.get("port_http"))
