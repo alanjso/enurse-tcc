849819bdba214a96578d2586b1717d4bccd0756f
Alan <alanoliveira1512@gmail.com>
2021-05-22 07:35:10 -0300

    Removendo tjbot desta api e migrando para API tts-stt que rodara com node v12

--- api/util/tjbot.js
+++ api/util/tjbot.js
@@ -1,15 +1,15 @@
-import TJBot from 'tjbot';
+//import TJBot from 'tjbot';
 
-console.log(TJBot.CAPABILITIES);
+//console.log(TJBot.CAPABILITIES);
 
-const tj = new TJBot({
-    credentialsFile="/home/pi/enurse-tcc/ibm.env",
-    robot: {
-        gender: TJBot.GENDERS.FEMALE
-    },
-    speak: {
-        language: TJBot.LANGUAGES.SPEAK.PORTUGUESE
-    }
-});
+//const tj = new TJBot({
+  //  credentialsFile="/home/pi/enurse-tcc/ibm.env",
+   // robot: {
+     //   gender: TJBot.GENDERS.FEMALE
+    //},
+    //speak: {
+     //   language: TJBot.LANGUAGES.SPEAK.PORTUGUESE
+   // }
+//});
 
-tj.initialize([TJBot.HARDWARE.LED_NEOPIXEL, TJBot.HARDWARE.SERVO, TJBot.HARDWARE.MICROPHONE, TJBot.HARDWARE.SPEAKER]);
+//tj.initialize([TJBot.HARDWARE.LED_NEOPIXEL, TJBot.HARDWARE.SERVO, TJBot.HARDWARE.MICROPHONE, TJBot.HARDWARE.SPEAKER]);
