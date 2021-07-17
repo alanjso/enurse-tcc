b740aa2dd0404f0da84c0a6c6810f32a6b042ad3
Alan Oliveira <alan.oliveira@g4flex.com.br>
2021-07-17 19:22:11 -0300

    Arquivo com o texto de ajuda para usar no /ajuda e /start

---
+++ api/util/textoAjuda.js
@@ -0,0 +1,19 @@
+let ajuda = "Olá, bem vindo(a) a EVA" + "\n";
+
+ajuda = ajuda + "Esses são os comandos que irão lhe ajudar:" + "\n";
+
+ajuda = ajuda + "/start ou /ajuda => Enviam lista de comandos da EVA" + "\n";
+
+ajuda = ajuda + "/getSinais => Retorna os valores atualizados dos sinais vitais do paciente" + "\n";
+
+ajuda = ajuda + "/tts frase => Toca um audio para o paciente com a frase escrita" + "\n";
+
+ajuda = ajuda + "/getConfig => Retorna os valores mínimos e máximos de cada sinal vital" + "\n";
+
+ajuda = ajuda + "/setBpm min|max => Configura o valor mínimo e máximo da Frequencia Cardiaca" + "\n";
+
+ajuda = ajuda + "/setSpo2 min|max => Configura o valor mínimo e máximo da " + "\n";
+
+ajuda = ajuda + "/setTemp min|max => Configura o valor mínimo e máximo da " + "\n";
+
+module.exports = ajuda
